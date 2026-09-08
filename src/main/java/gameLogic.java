d9907c4bdd37915aab52691e6740548834e1041c
larulscc-g-025 <larulscc-g-025@student.wethinkcode.co.za>
2026-09-08 14:43:49 +0200

    added all appropriate files and starting decks

---
+++ src/main/java/gameLogic.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class gameLogic {
+    public final String[] COLOURS = {"Red", "Blue", "Yellow", "Green"};
+    public final int[] NUMBERS = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    public final String[] WILD_CARDS = {"wild", "+4"};
+    public final String[] ACTION_CARDS = {"+2", "reverse", "block","skip"};
+    public ArrayList<String> deck = new ArrayList<>();
+    public ArrayList<String> discard_pile = new ArrayList<>();
+
+}
