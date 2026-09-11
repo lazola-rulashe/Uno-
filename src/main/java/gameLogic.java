a22dba4fc4b21369a83e231d6c96334af627975d
Lazola <larulscc-g-025@student.wethinkcode.co.za>
2026-09-11 14:52:26 +0200

    created a method that returns a list of all the action cards

--- src/main/java/gameLogic.java
+++ src/main/java/gameLogic.java
@@ -2,12 +2,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class gameLogic {
-//    public final String[] COLOURS = {"Red", "Blue", "Yellow", "Green"};
-    public final int[] NUMBERS = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    public final int[] NUMBERS = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     public final String[] WILD_CARDS = {"wild", "+4"};
-    public final String[] ACTION_CARDS = {"+2", "reverse", "block","skip"};
+    public final String[] ACTION_CARDS = {"+2", "reverse","skip"};
     public ArrayList<String> deck = new ArrayList<>();
-    public ArrayList<String> discard_pile = new ArrayList<>();
 
     public String getEachCard(String name, String colour){
         return name + ":" + colour;
@@ -21,9 +19,33 @@ public class gameLogic {
             numberCards.add(getEachCard(String.valueOf(num), "Blue"));
             numberCards.add(getEachCard(String.valueOf(num), "Yellow"));
             numberCards.add(getEachCard(String.valueOf(num), "Green"));
+
+            numberCards.add(getEachCard(String.valueOf(num), "Red"));
+            numberCards.add(getEachCard(String.valueOf(num), "Blue"));
+            numberCards.add(getEachCard(String.valueOf(num), "Yellow"));
+            numberCards.add(getEachCard(String.valueOf(num), "Green"));
+
         }
+
+        numberCards.add(getEachCard(String.valueOf(0), "Red"));
+        numberCards.add(getEachCard(String.valueOf(0), "Blue"));
+        numberCards.add(getEachCard(String.valueOf(0), "Yellow"));
+        numberCards.add(getEachCard(String.valueOf(0), "Green"));
 
         return numberCards;
     }
 
+    public ArrayList<String> getActionCards(){
+        ArrayList<String> actionCards= new ArrayList<>();
+
+        for (String action: ACTION_CARDS){
+            actionCards.add(getEachCard(action, "Red"));
+            actionCards.add(getEachCard(action, "Blue"));
+            actionCards.add(getEachCard(action, "Yellow"));
+            actionCards.add(getEachCard(action, "Green"));
+
+        }
+        return actionCards;
+
+    }
 }
