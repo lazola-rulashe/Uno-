f7fb6e1acf3a7279e09dcb66bfd50640b46a9a27
Lazola <larulscc-g-025@student.wethinkcode.co.za>
2026-09-11 14:23:27 +0200

    created method that returns the format of each card and a separate method that returns the list of all the number cards

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,11 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
+    public static void main(String[] args){
+        gameLogic sum = new gameLogic();
+        System.out.println(sum.getNumberCards());
+
+
+    }
 }
--- src/main/java/gameLogic.java
+++ src/main/java/gameLogic.java
@@ -1,11 +1,29 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class gameLogic {
-    public final String[] COLOURS = {"Red", "Blue", "Yellow", "Green"};
+//    public final String[] COLOURS = {"Red", "Blue", "Yellow", "Green"};
     public final int[] NUMBERS = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     public final String[] WILD_CARDS = {"wild", "+4"};
     public final String[] ACTION_CARDS = {"+2", "reverse", "block","skip"};
     public ArrayList<String> deck = new ArrayList<>();
     public ArrayList<String> discard_pile = new ArrayList<>();
+
+    public String getEachCard(String name, String colour){
+        return name + ":" + colour;
+    }
+
+    public ArrayList<String> getNumberCards(){
+        ArrayList<String> numberCards = new ArrayList<>();
+
+        for (int num : NUMBERS) {
+            numberCards.add(getEachCard(String.valueOf(num), "Red"));
+            numberCards.add(getEachCard(String.valueOf(num), "Blue"));
+            numberCards.add(getEachCard(String.valueOf(num), "Yellow"));
+            numberCards.add(getEachCard(String.valueOf(num), "Green"));
+        }
+
+        return numberCards;
+    }
 
 }
