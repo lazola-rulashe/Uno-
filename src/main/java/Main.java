663ba5930076cba63184f342d719cfca1f378f5e
Lazola <larulscc-g-025@student.wethinkcode.co.za>
2026-09-12 14:44:01 +0200

    Removed all server and client logic

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,60 +6,28 @@ import java.util.Stack;
 
 public class Main {
 
-    public static ArrayList<String> player1Hand = new ArrayList<>();
-    public static ArrayList<String> player2Hand = new ArrayList<>();
     public static ArrayList<String> discardPile = new ArrayList<>();
 
 
     public static void main(String[] args) throws ClassNotFoundException, IOException {
 
-        ServerSocket s = new ServerSocket( Server.PORT);
         System.out.println("Welcome to UNO!");
-        while(true) {
-            try {
-                Socket socket = s.accept();
-//                System.out.println("Connection: " + socket.getInetAddress().getCanonicalHostName());
+        DeckHandler deck = new DeckHandler();
 
-                Runnable r = new Server(socket);
-                Thread task = new Thread(r);
-                task.start();
+        ArrayList<String> action = deck.getActionCards();
+        ArrayList<String> wild = deck.getWildCards();
+        ArrayList<String> Deck = deck.getDeck();
 
+        StringBuilder str = new StringBuilder();
 
-                DeckHandler deck = new DeckHandler();
-
-                ArrayList<String> action = deck.getActionCards();
-                ArrayList<String> wild = deck.getWildCards();
-                ArrayList<String> Deck = deck.getDeck();
-
-                StringBuilder str = new StringBuilder();
-
-                for (int i = 0; i < Deck.toArray().length; i++){
-                    if(!(wild.contains(Deck.get(i)) || action.contains(Deck.get(i)))){
-                        str.append(Deck.get(i));
-                        discardPile.add(str.toString());
-                        Deck.remove(str.toString());
-                        break;
-                    }
-                }
-
-
-
-            } catch(IOException ex) {
-                ex.printStackTrace();
+        for (int i = 0; i < Deck.toArray().length; i++){
+            if(!(wild.contains(Deck.get(i)) || action.contains(Deck.get(i)))){
+                str.append(Deck.get(i));
+                discardPile.add(str.toString());
+                Deck.remove(str.toString());
+                break;
             }
         }
-
-
-
-//        System.out.println(discardPile);
-//        System.out.println(Deck);
-//        System.out.println(player1Hand);
-//        System.out.println(player2Hand);
-//        System.out.println(Deck.size());
-
-
-
-
 
     }
 }
---
+++ src/main/java/Play.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Play {
+
+
+
+    public ArrayList<String> deal(){
+        ArrayList<String> playerHand = new ArrayList<>();
+        DeckHandler deck = new DeckHandler();
+
+        ArrayList<String> Deck = deck.getDeck();
+
+        for (int i = 0; i < Deck.toArray().length; i++){
+            playerHand.add(Deck.get(i));
+            Deck.remove(Deck.get(i));
+            if(playerHand.size() == 8) {
+                break;
+            }
+        }
+        return playerHand;
+    }
+
+
+
+}
