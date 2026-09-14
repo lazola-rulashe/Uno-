fe6b25c8cf352c237a3cca030cea9436ab275db2
Lazola <larulscc-g-025@student.wethinkcode.co.za>
2026-09-14 08:52:30 +0200

    Added test for returning a list of number cards

--- src/tests/java/DeckHandlerTest.java
+++ src/tests/java/DeckHandlerTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
+import java.util.Objects;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DeckHandlerTest {
@@ -20,6 +22,52 @@ public class DeckHandlerTest {
         assertEquals("reverse:Blue", actionCard);
         assertNotEquals(4+":red", numberCard);
         assertNotEquals("Reverse:red", actionCard);
+
+    }
+
+    @Test
+    public void shouldReturnListOfNumberCards(){
+
+        //Total length of all number cards
+        assertEquals(76, deck.getNumberCards().toArray().length);
+
+        //Tests the number of zeros there are
+        int count = 0;
+        for(String num: deck.getNumberCards()){
+            if(num.contains("0")){
+                count++;
+            }
+        }
+        assertEquals(4, count);
+        assertFalse(count < 4);
+        assertFalse(count > 4);
+
+        //Tests that there are two cards per colour from 1-9
+        int counts = 0;
+
+        for(String num: deck.getNumberCards()){
+            if(num.contains("1")){
+                counts++;
+            }
+            if(num.contains("2")){
+                counts++;
+            }if(num.contains("3")){
+                counts++;
+            }if(num.contains("4")){
+                counts++;
+            }if(num.contains("5")){
+                counts++;
+            }if(num.contains("6")){
+                counts++;
+            }if(num.contains("7")){
+                counts++;
+            }if(num.contains("8")){
+                counts++;
+            }if(num.contains("9")){
+                counts++;
+            }
+        }
+        assertEquals(72, counts);
 
     }
 }
